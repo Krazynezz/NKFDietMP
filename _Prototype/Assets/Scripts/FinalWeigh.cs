3e29df9de505396966365633b9c4d1cfe8cba0f7
eeshik <ee_shi_kiat@yahoo.com.sg>
2021-08-31 19:31:20 +0800

    weijie weights

--- _Prototype/Assets/Scripts/FinalWeigh.cs
+++ _Prototype/Assets/Scripts/FinalWeigh.cs
@@ -7,14 +7,17 @@ public class FinalWeigh : MonoBehaviour
     float num = 0;
     public GameObject connectdoor;
     public GameObject connectdoor2;
-    public GameObject connectdoor3;
     public GameObject[] root;
     public GameObject player;
-
+    static bool[] weighs = new bool[] { false,false,false };
+    static int weighindex = 0;
+    static int complete = 0;
+    public Vector3 weighplate;
+    Vector3 off;
     // Start is called before the first frame update
     void Start()
     {
-
+        off = new Vector3(0.4f, 0, 0);
     }
 
     // Update is called once per frame
@@ -22,10 +25,27 @@ public class FinalWeigh : MonoBehaviour
     {
         if (num == 2)
         {
+            weighs[weighindex] = true;
+            weighindex++;
+            num++;
+        }
+        foreach (var item in weighs)
+            {
+            if (item == true)
+            {
+                complete++;
+                Debug.Log(complete);
+            }
+            if (item == false)
+            {
+                complete = 0;
+            }
+            }
+        if (complete ==3)
+        {
             Destroy(this.gameObject);
             connectdoor.GetComponent<Animator>().SetBool("character_nearby", true);
             connectdoor2.GetComponent<Animator>().SetBool("character_nearby", true);
-            connectdoor3.GetComponent<Animator>().SetBool("character_nearby", true);
         }
     }
 
@@ -35,7 +55,7 @@ public class FinalWeigh : MonoBehaviour
         if (other.tag == "interactable" )
         {
             player.GetComponent<move>().interacted = false;
-            other.transform.position = this.gameObject.transform.position + Vector3.up;
+            other.transform.position = weighplate + off *(num-1);
             other.transform.rotation = Quaternion.Euler(0, 0, 0);
         }
         foreach (var item in root)
