a4796252fadd6266b993419092ebe6904883aecf
Javier Lai <javier.lai@yahoo.com.sg>
2021-09-03 03:06:08 +0800

    Minor changes to scene.

--- _Prototype/Assets/Replicator.cs
+++ _Prototype/Assets/Replicator.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class replcate : MonoBehaviour
+public class Replicator : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
