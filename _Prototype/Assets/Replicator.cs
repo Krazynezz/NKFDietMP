5919d4292933c459f34368eb791f5fe93c6075a6
Javier Lai <javier.lai@yahoo.com.sg>
2021-09-07 23:55:22 +0800

    Updated replicator script.

--- _Prototype/Assets/Replicator.cs
+++ _Prototype/Assets/Replicator.cs
@@ -7,10 +7,23 @@ using UnityEngine.Events;
 public class Replicator : MonoBehaviour
 {
     [HideInInspector]
-    public Placement placement;
+    public canister canister;
+    bool platePlaced = false;
 
     private void OnMouseDown()
     {
         Debug.Log("Button Clicked");
+        if (canister.cancount == 3 && platePlaced == true)
+        {
+            Debug.Log("3 canisters placed");
+        }
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "PlateOne" || other.name == "PlateTwo" || other.name == "PlateThree")
+        {
+            Debug.Log("Hello");
+        }
     }
 }
