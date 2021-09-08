1996546010a2826d12c5fcb63ebd992833371486
Javier Lai <javier.lai@yahoo.com.sg>
2021-09-08 19:22:23 +0800

    Made the replicator replicate and instantiate a full plate.

--- _Prototype/Assets/Replicator.cs
+++ _Prototype/Assets/Replicator.cs
@@ -10,18 +10,48 @@ public class Replicator : MonoBehaviour
     public canister canister;
     [HideInInspector]
     public bool platePlaced = false;
+    [HideInInspector]
+    public bool replicatable = false;
+
+    public Transform outPutPos;
+
+    public Rigidbody plateOneFull;
+    public Rigidbody plateTwoFull;
+    public Rigidbody plateThreeFull;
 
     private void OnTriggerStay(Collider other)
     {
         if (other.name == "PlateOne" || other.name == "PlateTwo" || other.name == "PlateThree")
         {
             platePlaced = true;
-            Debug.Log("Plate placed: "+ other.gameObject.name);  
+            Debug.Log("Plate placed: " + other.gameObject.name);
+            if (replicatable == true)
+            {
+                if(other.name == "PlateOne")
+                {
+                    Instantiate(plateOneFull, outPutPos.position, outPutPos.rotation);
+                    //Destroy(other.gameObject);
+                }
+                else if (other.name == "PlateTwo")
+                {
+                    Instantiate(plateTwoFull, outPutPos.position, outPutPos.rotation);
+                    //Destroy(other.gameObject);
+                }
+                else if (other.name == "PlateThree")
+                {
+                    Instantiate(plateThreeFull, outPutPos.position, outPutPos.rotation);
+                    //Destroy(other.gameObject);
+                }
+                Destroy(other.gameObject);
+                replicatable = false;
+                platePlaced = false;
+            }
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
+        replicatable = false;
         platePlaced = false;
     }
 }
--- _Prototype/Assets/ReplicatorButton.cs
+++ _Prototype/Assets/ReplicatorButton.cs
@@ -5,22 +5,27 @@ using UnityEngine;
 public class ReplicatorButton : MonoBehaviour
 {
     public Replicator replicator;
+    
 
     private void OnMouseDown()
     {
-        /*
         Debug.Log(canister.cancount);
-        Debug.Log("Button Clicked");
-        if (canister.cancount == 2 && replicator.platePlaced == true)
+        //Debug.Log("Button Clicked");
+        if (canister.cancount >= 2 && replicator.platePlaced == true)
         {
-            Debug.Log("3 canisters placed");
+            replicator.replicatable = true;
+            Debug.Log("Replicating!");
         }
         else
         {
             Debug.Log("Something is missing");
         }
-        */
-        Debug.Log(replicator.platePlaced);
-
+        //Debug.Log(replicator.platePlaced);
     }
+
+    /*
+    private void OnMouseUp()
+    {
+        replicator.replicatable = false;
+    }*/
 }
