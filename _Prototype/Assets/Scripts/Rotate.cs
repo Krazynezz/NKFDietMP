3c748ec42271c5d23e83c83cc545f7961ded17d0
Javier Lai <javier.lai@yahoo.com.sg>
2021-09-14 17:12:45 +0800

    Put back rotate script.

---
+++ _Prototype/Assets/Scripts/Rotate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    /*
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 360f, 0), Time.deltaTime * 45, Space.World);
+    }
+    */
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Placement")
+        {
+            transform.Rotate(new Vector3(0, 360f, 0), Time.deltaTime * 45, Space.World);
+        }
+    }
+
+}
