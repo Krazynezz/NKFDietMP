dcaf4d9368dec85832282d4b9ab120527ecba08b
eeshik <ee_shi_kiat@yahoo.com.sg>
2021-09-15 19:30:17 +0800

    scriptable object

--- _Prototype/Assets/Scripts/Insertion.cs
+++ _Prototype/Assets/Scripts/Insertion.cs
@@ -6,4 +6,6 @@ using UnityEngine;
     public class Insertion : ScriptableObject
 {
         public GameObject[] interactables;
+    //            insertion.interactables = GameObject.FindGameObjectsWithTag("interactable");
+
 }
--- _Prototype/Assets/Scripts/intro.cs
+++ _Prototype/Assets/Scripts/intro.cs
@@ -17,7 +17,6 @@ public class intro : MonoBehaviour
     {
             if (outlined == false)
         {
-            insertion.interactables = GameObject.FindGameObjectsWithTag("interactable");
             foreach (var item in insertion.interactables)
             {
                 item.AddComponent<origin>();
