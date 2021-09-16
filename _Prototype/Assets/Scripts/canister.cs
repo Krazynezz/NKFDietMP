5148a06a19ab362d7d9b18c66e1d89edadf22a3a
eeshik <ee_shi_kiat@yahoo.com.sg>
2021-09-17 02:58:36 +0800

    comments

--- _Prototype/Assets/Scripts/canister.cs
+++ _Prototype/Assets/Scripts/canister.cs
@@ -7,38 +7,36 @@ public class canister : MonoBehaviour
     static public float cancount;
     public GameObject player;
     public Vector3 end;
-    Vector3 off;
     // Start is called before the first frame update
     void Start()
     {
-        cancount = -1;
-        off = Vector3.left/5;
+        cancount = -1;      //tracks the number of canisters on the replicator
     }
 
     // Update is called once per frame
     void Update()
     {
-                    if (cancount >= 0)
+        if (cancount >= 0)          //if the is at least one canister on the replicator
             {
-                this.gameObject.GetComponent<AudioSource>().clip = null;
+                this.gameObject.GetComponent<AudioSource>().clip = null;        //the audio source attached to this object will be null so that the audio will not play again
             }
     }
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other)         
     {
-        if (other.tag == "replicator")
+        if (other.tag == "replicator")              //when this object collides with the replicator object
         {
-            player.GetComponent<move>().interacted = false;
-            this.gameObject.transform.position = end;
-            this.gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);
-            cancount += 1;
+            player.GetComponent<move>().interacted = false;         //the player will stop holding this object
+            this.gameObject.transform.position = end;               //this object will move to the coordinates asign in inspector
+            this.gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);         //and be vertically placed
+            cancount += 1;          //and increase the tracker for number of canister
 
         }
     }
     private void OnTriggerExit(Collider other)
     {
-        if (other.tag == "replicator")
+        if (other.tag == "replicator")          //if the canisters are removed 
         {
-            cancount--;
+            cancount--;             //the tracker will decrease
         }
     }
 }
--- _Prototype/Assets/Scripts/plate.cs
+++ _Prototype/Assets/Scripts/plate.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class plate : MonoBehaviour
 {
     public Replicator replicator;
-    Vector3 origin;
     public bool divisionright = false;
     GameObject player;
     public GameObject door;
@@ -14,9 +13,8 @@ public class plate : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        origin = transform.position;
-        door = GameObject.Find("Cafeteria_Exit");
-        player = Camera.main.gameObject;
+        door = GameObject.Find("Cafeteria_Exit");       //assign the door to open
+        player = Camera.main.gameObject;                //and assign the player camera when this object is spawned
     }
 
     // Update is called once per frame
@@ -27,30 +25,30 @@ public class plate : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "replicator" && replicator.platePlaced == false)
+        if (other.tag == "replicator" && replicator.platePlaced == false)           //if this object collides with the replicator object and it does not have a plate inside already
         {
-            player.GetComponent<move>().interacted = false;
-            this.gameObject.transform.position = other.transform.position;
+            player.GetComponent<move>().interacted = false;                     //the player stops holding this object
+            this.gameObject.transform.position = other.transform.position;      //this plate will then move to inside the replicator
         }
-            if (other.tag == "destination")
+            if (other.tag == "destination")         //if this plate collides with the object with the destination tag
             {
-                player.GetComponent<move>().interacted = false;
-            if (divisionright == false)
+                player.GetComponent<move>().interacted = false;      //the player stops holding this object
+            if (divisionright == false)                 //if this plate is the correct answer with the correct perportions as the healthy plate
             {
-                this.gameObject.transform.position = origin;
-                this.gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);
+                this.gameObject.transform.position = this.gameObject.GetComponent<origin>().originpos;            //if it is not the right plate then it will return to its original position
+                this.gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);                 //and original rotation
             }
-            if (divisionright == true)
-            {
-                eaten = true;    
-                if (bottle.hydate == true)
+            if (divisionright == true)              //if it is the right plate 
                 {
-                    door.GetComponent<Animator>().SetBool("character_nearby", true);
+                    eaten = true;               //one of the conditions to open the door will become true
+                if (bottle.hydate == true)              //if the other condition for opening the door is true
+                {
+                    door.GetComponent<Animator>().SetBool("character_nearby", true);            //the door will open
                 }
-                player.GetComponent<move>().interacted = false;
-                Destroy(this.gameObject);
+                player.GetComponent<move>().interacted = false;             //the player will stop holding this plate
+                Destroy(this.gameObject);                   //and this gameobject will be destroyed
 
-            }
+                }
 
         }
 
