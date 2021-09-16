3e7b57958d9690593cca8a6e3bd24b662f9ab254
eeshik <ee_shi_kiat@yahoo.com.sg>
2021-09-17 03:17:56 +0800

    comments

--- _Prototype/Assets/Scripts/Placement.cs
+++ _Prototype/Assets/Scripts/Placement.cs
@@ -8,7 +8,7 @@ public class Placement : MonoBehaviour
     public GameObject Waterbottle;
     public GameObject player;
     public GameObject container;
-    bool done = false;
+    private bool done = false;
     static float canned;
     public string correct;
 
@@ -23,25 +23,23 @@ public class Placement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (canned == 5)
+        if (canned == 5)                //if all cans have been placed correctly 
         {
-            Instantiate(Waterbottle, container.transform.position, Quaternion.identity);
-            canned += 1;
+            Instantiate(Waterbottle, container.transform.position, Quaternion.identity);        //then the water that is part of a healthy meal will spawn
+            canned += 10;            //and the tracker for the cans will increase so that it will not spawn another water
         }
     }
     private void OnTriggerEnter(Collider item)
-    {
-        
-    
-               if (item.tag == "interactable" && done != true)
-                {
-                    player.GetComponent<move>().interacted = false;
-                    item.transform.position = transform.position + Vector3.up/5;
-                    item.transform.rotation = Quaternion.Euler(0, -90, 0);
-                    done = true;
-            if (item.name == correct)
+    {        
+        if (item.tag == "interactable" && done != true)                    //when this object collides with an interactable object and this position does not have a can already
+        {
+                    player.GetComponent<move>().interacted = false;         //the player will stop holding the object
+                    item.transform.position = transform.position + Vector3.up/5;            //and the object will move to above this position 
+                    item.transform.rotation = Quaternion.Euler(0, -90, 0);          //and face the edge of the table
+                    done = true;                    //and this position will have a can already in case another can is trying to be placed here
+            if (item.name == correct)           //if the name of the object placed in this position is the same as the intended name
             {
-                canned += 1;
+                canned += 1;                //then it will increase the number of correct cans that have been placed by one
             } 
         }
             
--- _Prototype/Assets/Scripts/Placement2.cs
+++ _Prototype/Assets/Scripts/Placement2.cs
@@ -8,8 +8,6 @@ public class Placement2 : MonoBehaviour
     public GameObject Item;
     public GameObject player;
     public GameObject connectdoor;
-    RaycastHit hit;
-    //public bool done = false;
     static public float correctItems;
 
     // Start is called before the first frame update
@@ -21,21 +19,21 @@ public class Placement2 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (correctItems == 3)
+        if (correctItems == 3)          //all three figurines have placed correctly
         {
-            connectdoor.GetComponent<Animator>().SetBool("character_nearby",true);
+            connectdoor.GetComponent<Animator>().SetBool("character_nearby",true);              //then door attached should open
         }
     }
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "interactable")
+        if (other.tag == "interactable")        //if an interactable object collides with this object
         {
-            player.GetComponent<move>().interacted = false;
-            other.transform.position = this.gameObject.transform.position + Vector3.up/ 100;
-            other.transform.rotation = Quaternion.Euler(0, 0, 0);
-            if (other.gameObject == Item)
+            player.GetComponent<move>().interacted = false;         //the player will stop holding the object
+            other.transform.position = this.gameObject.transform.position + Vector3.up/ 100;        //and be placed above this platform
+            other.transform.rotation = Quaternion.Euler(0, 0, 0);           //at the original rotation
+            if (other.gameObject == Item)           //if the item is the same as the intended object assigned in inspector
             {
-                correctItems += 1;
+                correctItems += 1;              //the number of correct objects will increase
             }
         }
     }
