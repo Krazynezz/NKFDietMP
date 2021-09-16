63a55cb1b65bee77770b4b2fab6a9612b7a4d5be
eeshik <ee_shi_kiat@yahoo.com.sg>
2021-09-17 00:57:51 +0800

    audio souce deleted

--- _Prototype/Assets/Scripts/audioManager.cs
+++ _Prototype/Assets/Scripts/audioManager.cs
@@ -7,11 +7,11 @@ public class audioManager : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "MainCamera" && this.gameObject.GetComponent<AudioSource>().clip != null)
+        if (other.tag == "MainCamera" && this.gameObject.GetComponent<AudioSource>().clip != null)          //when the player moves into one of the trigger areas
         {
-            other.GetComponent<AudioSource>().clip = this.gameObject.GetComponent<AudioSource>().clip;
-            other.GetComponent<AudioSource>().Play();
-            this.gameObject.GetComponent<AudioSource>().clip = null;
+            other.GetComponent<AudioSource>().clip = this.gameObject.GetComponent<AudioSource>().clip;          //the current audio clip will be cut off and replaced with the one attached to this object
+            other.GetComponent<AudioSource>().Play();                           //and start plaing  
+            this.gameObject.GetComponent<AudioSource>().clip = null;            //and the clip attached to this object will be removed to prevent it from playing again if triggered again
         }
     }
 }
