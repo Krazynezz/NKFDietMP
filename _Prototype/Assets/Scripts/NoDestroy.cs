e82e3bf826165f375678bc560e0b627a8178a510
eeshik <ee_shi_kiat@yahoo.com.sg>
2021-09-17 00:48:35 +0800

    owen audio

--- _Prototype/Assets/Scripts/NoDestroy.cs
+++ _Prototype/Assets/Scripts/NoDestroy.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class NoDestroy : MonoBehaviour
 {
+    AudioClip startingclip;
     private static NoDestroy gameInstance;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.gameObject.GetComponent<AudioSource>().clip = startingclip;
         DontDestroyOnLoad(this.gameObject); // This will prevent the music object from being destroyed upon loading into another scene
 
         if (gameInstance == null) // This will prevent the BGM from playing twice upon loading back to the start menu screen
