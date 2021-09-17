a7100b0dc98749cb5968fc4b62c1f5b7ea046f58
eeshik <ee_shi_kiat@yahoo.com.sg>
2021-09-17 17:27:09 +0800

    comments

--- _Prototype/Assets/Scripts/curvepipe.cs
+++ _Prototype/Assets/Scripts/curvepipe.cs
@@ -13,11 +13,11 @@ public class curvepipe : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        int rand = Random.Range(0, rotations.Length);
-        transform.RotateAround(this.gameObject.GetComponent<Collider>().bounds.center, Vector3.forward, rotations[rand]);
+        int rand = Random.Range(0, rotations.Length);       //a random 90 degrees is set
+        transform.RotateAround(this.gameObject.GetComponent<Collider>().bounds.center, Vector3.forward, rotations[rand]);                      //all pipes will be rotated to the random 90 degrees when spawned
         foreach (Transform child in transform)
         {
-            waypoints[i] = child;
+            waypoints[i] = child;               //each pipe has a list of empty game objects that is gotten and assigned to be referenced later
             i++;
 
         }
@@ -29,50 +29,46 @@ void Update()
     {
 
     }
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other)         //when a sphere collides and enters the pipe
     {
-        other.GetComponent<flowing>().enabled = false;
+        other.GetComponent<flowing>().enabled = false;          //it will stop moving by itself
         i = 0;
-        foreach (var item in waypoints)
+        foreach (var item in waypoints)     //and the list of empty gameobject in the children is used
         {
-            distance[i] = Vector3.Distance(other.transform.position, item.transform.position);
-            i++;
+            distance[i] = Vector3.Distance(other.transform.position, item.transform.position);          //to find the difference between sphere position and each empty game object
+            i++;            //and is saved in a seperate array
         }
     }
-    private void OnTriggerStay(Collider other)
+    private void OnTriggerStay(Collider other)          //while the sphere is in the pipe
     {
-        this.tag = "Untagged";
-        if (Mathf.Min(distance) == distance[1])
+        this.tag = "Untagged";              //this pipe cannot be rotated since the rotation requires the tag
+        if (Mathf.Min(distance) == distance[1])         //and the smallest difference between sphere position and each empty game object is used to determine which end of the pipe the sphere is at
         {
             {
-                other.transform.position = Vector3.MoveTowards(other.transform.position, waypoints[head].position, 0.001f);
-                if (Vector3.Distance(other.transform.position, waypoints[head].position) < 0.001)
+                other.transform.position = Vector3.MoveTowards(other.transform.position, waypoints[head].position, 0.001f);         //and the sphere will move towards the closest waypoint
+                if (Vector3.Distance(other.transform.position, waypoints[head].position) < 0.001)           //until it is close enough to that waypoint
                 {
-                    head++;
-                }
-                if(head >=6)
-                {
-                    
+                    head++;             //then it start to move to the next waypoint
                 }
             }
         }
-        if (Mathf.Min(distance) == distance[4])
+        if (Mathf.Min(distance) == distance[4])             //if the sphere is at the other end of the pipe
         {
-            other.transform.position = Vector3.MoveTowards(other.transform.position, waypoints[tail].position, 0.001f);
+            other.transform.position = Vector3.MoveTowards(other.transform.position, waypoints[tail].position, 0.001f);         //then it will move in the othe direction
             if (Vector3.Distance(other.transform.position, waypoints[tail].position) < 0.001)
             {
                 tail--;
             }         
         }
-        if(Mathf.Min(distance) == distance[2]|| Mathf.Min(distance) == distance[3])
+        if(Mathf.Min(distance) == distance[2]|| Mathf.Min(distance) == distance[3])         //if the sphere is not close to the two ends of the pipe
         {
-            Destroy(other.gameObject);
+            Destroy(other.gameObject);          //it is not entering the pipe at the right ends so it is destroyed
         }
         }
-    private void OnTriggerExit(Collider other)
+    private void OnTriggerExit(Collider other)              //when the sphere leaves the pipe
     {
         tail = 4;
-        head = 1;
-        this.tag = "pipe";
+        head = 1;               //the variables are reset
+        this.tag = "pipe";      //and the pipe can be rotated again
     }
 }
--- _Prototype/Assets/Scripts/move.cs
+++ _Prototype/Assets/Scripts/move.cs
@@ -33,19 +33,17 @@ public class move : MonoBehaviour
     // Start is called before the first frame update
     void Start()
         {
-        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.lockState = CursorLockMode.Locked;           //the mouse position is locked to the middle of the screen
         }
 
     // Update is called once per frame
     private void FixedUpdate()
     {
-
-           front = transform.forward * Input.GetAxis("Vertical");
-        left = transform.right * Input.GetAxis("Horizontal");
-        movement = front + left;
-        movement.y = 0;
-        player.Move(movement / 20);
-        player.transform.position = new Vector3(transform.position.x,-1.1f,transform.position.z);
+        front = transform.forward * Input.GetAxis("Vertical");          //gets the vertical inputs of the user
+        left = transform.right * Input.GetAxis("Horizontal");           //gets the horizontal inputs of the user
+        movement = front + left;                        //adds the two inputs together
+        player.Move(movement / 20);             //moves the player according to their input at a lower rate
+        player.transform.position = new Vector3(transform.position.x,-1.1f,transform.position.z);           //resets the height of the player if he tries to climb an object
 
     }
 
@@ -53,92 +51,91 @@ public class move : MonoBehaviour
 
     void Update()
     {
-        if (maze == true)
+        if (maze == true)           //if the maze has been cleared
         {
-            foreach (var item in mazeshader)
+            foreach (var item in mazeshader)        //the two components of the patient hologram
             {
-                item.GetComponent<Renderer>().material = greenshader;
+                item.GetComponent<Renderer>().material = greenshader;           //will turn green
             }
         }
-        if (pipes == true)
+        if (pipes == true)          //if the pipes has been cleared
         {
-            foreach (var item in pipeshader)
+            foreach (var item in pipeshader)               //the two components of the patient hologram
             {
-                item.GetComponent<Renderer>().material = greenshader;
+                item.GetComponent<Renderer>().material = greenshader;      //will turn green
             }
         }
-        if(maze == true && pipes == true)
+        if(maze == true && pipes == true)           //if both conditions have been cleared
         {
-            teleporter.SetActive(true);
+            teleporter.SetActive(true);         //the teleporter will appear
         }
 
-        transform.eulerAngles += new Vector3(Input.GetAxis("Mouse Y") * -1, Input.GetAxis("Mouse X"), 0);
+        transform.eulerAngles += new Vector3(Input.GetAxis("Mouse Y") * -1, Input.GetAxis("Mouse X"), 0);           //changes the rotation of the camera based on mouse movement input
 
 
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out objects, 1))
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);                //shoots a ray from the middle of the screen since the mouse is locked
+            if (Physics.Raycast(ray, out objects, 1))           //of distance 1
             {
-            if (objects.collider.tag == "interactable")
+            if (objects.collider.tag == "interactable")             //if it hits an interactable object
             {
-                crosshair.SetActive(false);
-                crosshand.SetActive(true);
+                crosshair.SetActive(false);         //disables the crosshair 
+                crosshand.SetActive(true);          //and displays a hand
             }
-            if (Input.GetButtonDown("Fire1") && interacted == false)
+            if (Input.GetButtonDown("Fire1") && interacted == false)            //if the player clicks and is not already holding an object
             {
-                if (objects.collider.gameObject.GetComponent<AudioSource>() != null)
+                if (objects.collider.gameObject.GetComponent<AudioSource>().clip != null)            //if the object has audio source component attached and audio clip
                 {
-                    this.gameObject.GetComponent<AudioSource>().clip = objects.collider.gameObject.GetComponent<AudioSource>().clip;
+                    this.gameObject.GetComponent<AudioSource>().clip = objects.collider.gameObject.GetComponent<AudioSource>().clip;            //the player audio source will play its clip
                     this.gameObject.GetComponent<AudioSource>().Play();
-                    objects.collider.gameObject.GetComponent<AudioSource>().clip = null;
+                    objects.collider.gameObject.GetComponent<AudioSource>().clip = null;            //and the object will no longer have a clip attached
                 }
-                if (objects.collider.tag == "interactable")
+                if (objects.collider.tag == "interactable")         //if the object is interactable
                 {
-                    origin =  objects.collider.gameObject.GetComponent<origin>().originpos;
-                    originrot = objects.transform.rotation;
-                    interacting = objects.collider.gameObject;
-                    interacted = true;
+                    origin =  objects.collider.gameObject.GetComponent<origin>().originpos;     //the player saves its original position from its origin script
+                    originrot = objects.transform.rotation;         //and original rotation
+                    interacting = objects.collider.gameObject;          //and declares that what object it is holding
+                    interacted = true;              //and that it is holding an object
                 }
-                if (objects.collider.tag == "hemo" && !SceneManager.GetSceneByName("Pipes").isLoaded)
+                if (objects.collider.tag == "hemo" && !SceneManager.GetSceneByName("Pipes").isLoaded)           //if the player clicks on the hemodialysis machine
                 {
-                    Cursor.lockState = CursorLockMode.None;
-                    SceneManager.LoadScene("Pipes", LoadSceneMode.Additive);
-                    crosshair.SetActive(false);
+                    Cursor.lockState = CursorLockMode.None;         //the mouse is unlocked
+                    SceneManager.LoadScene("Pipes", LoadSceneMode.Additive);            //and the pipe scene is loaded
+                    crosshair.SetActive(false);         //and the crosshair disappears
 
 
                 }
-                if (objects.collider.tag == "peritonial" && !SceneManager.GetSceneByName("Maze").isLoaded)
+                if (objects.collider.tag == "peritonial" && !SceneManager.GetSceneByName("Maze").isLoaded)              //if the player clicks on the peritonial dialysis machine
                 {
-                    SceneManager.LoadScene("Maze", LoadSceneMode.Additive);
-                    crosshair.SetActive(false);
+                    SceneManager.LoadScene("Maze", LoadSceneMode.Additive);         //the maze scene is loaded
+                    crosshair.SetActive(false);             //and the crosshair disappears
 
                 }
-                if (objects.collider.tag == "button")
+                if (objects.collider.tag == "button")               //if the player clicks on keypad buttons
                 {
-                    input[order] = objects.collider.name;
-                    keylights[order].GetComponent<MeshRenderer>().material.color = Color.green;
-                    order += 1;
-                    Debug.Log(input[order - 1]);
-                    if (order == 4)
+                    input[order] = objects.collider.name;           //the array will store the number pressed
+                    keylights[order].GetComponent<MeshRenderer>().material.color = Color.green;         //and the corresponding indicator will turn green
+                    order += 1;             //and go to the next number to be pressed
+                    if (order == 4)             //if the player has clicked all numbers
                     {
                         for (int i = 0; i < 4; i++)
                         {
-                            if (input[i] == rightans[i])
+                            if (input[i] == rightans[i])            //if each number in the position matches with the right answer
                             {
-                                answered += 1;
+                                answered += 1;          //the number of correct answer the play has entered incrceases by one
                             }
                         }
-                        if (answered == 4)
+                        if (answered == 4)          //if all four answers are correct
                         {
-                            Instantiate(redcan, container.transform.position, Quaternion.Euler(0,90,0));
+                            Instantiate(redcan, container.transform.position, Quaternion.Euler(0,90,0));        //then the drinks prefab is spawned
                         }
-                        if (answered < 4)
+                        if (answered < 4)           //if there is at least one wrong answer
                         {
-                            foreach (var item in keylights)
+                            foreach (var item in keylights)         //gets all the indcators
                             {
-                                item.GetComponent<MeshRenderer>().material.color = Color.red;
+                                item.GetComponent<MeshRenderer>().material.color = Color.red;           //all of the indicators will turn red
                             }
-                            order = 0;
-                            answered = 0;
+                            order = 0;          //and the input will go back to the first position to wait for next round of input
+                            answered = 0;           //and the number of inputs will reset
                         }
                     }
                 }
@@ -146,21 +143,21 @@ public class move : MonoBehaviour
             }
        else
         {
-            crosshair.SetActive(true);
-            crosshand.SetActive(false);
+            crosshair.SetActive(true);          //if the ray does not hit an interacterble object then the crosshair will appear
+            crosshand.SetActive(false);         //and the hand will disappear
         }
-            if (Input.GetButtonDown("Fire2") && interacted != false)
+            if (Input.GetButtonDown("Fire2") && interacted != false)        //if the player right clicks
             {
-                interacting.transform.position = origin;
-                interacting.transform.rotation = originrot;
-                interacting = null;
-                interacted = false;
-            }
+                interacting.transform.position = origin;            //the object will move back to its original position
+                interacting.transform.rotation = originrot;         //and rotation
+                interacting = null;             //and declare that no object is being held
+                interacted = false;             //and declare that it is no longer holding an object
+        }
 
-        if (interacted)
-            {
-                interacting.transform.position = this.transform.position + transform.forward - transform.up/10;
-                interacting.transform.rotation = this.transform.rotation * Quaternion.Euler(-10,0,0);
+        if (interacted)            //if the player is holding an object 
+        {
+                interacting.transform.position = this.transform.position + transform.forward - transform.up/10;         //the object will move based on the player position and be in front of the player
+                interacting.transform.rotation = this.transform.rotation * Quaternion.Euler(-10,0,0);               //and its rotation will also follow so that it always faces the player
             }
         }
     }
